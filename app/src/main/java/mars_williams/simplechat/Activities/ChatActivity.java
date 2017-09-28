eacee353639d7991fb36cab65f6f35a1f7ad5618
Mars Willilams <mars.williams@airbnb.com>
2017-09-28 09:58:39 -0700

    bound messages to list in recyclerview;

--- app/src/main/java/mars_williams/simplechat/Activities/ChatActivity.java
+++ app/src/main/java/mars_williams/simplechat/Activities/ChatActivity.java
@@ -2,6 +2,8 @@ package mars_williams.simplechat.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +16,9 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.util.ArrayList;
+
+import mars_williams.simplechat.Adapters.ChatAdapter;
 import mars_williams.simplechat.Models.Message;
 import mars_williams.simplechat.R;
 
@@ -27,6 +32,11 @@ public class ChatActivity extends AppCompatActivity {
     EditText etMessage;
     Button btSend;
 
+    RecyclerView rvChat;
+    ArrayList<Message> mMessages;
+    ChatAdapter mAdapter;
+    // Keep track of initial load to scroll to the bottom of the ListView
+    boolean mFirstLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,10 +68,23 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Setup message field and posting
     void setupMessagePosting() {
         // Find the text field and button
         etMessage = findViewById(R.id.etMessage);
         btSend = findViewById(R.id.btSend);
+        rvChat = findViewById(R.id.rvChat);
+        mMessages = new ArrayList<>();
+        mFirstLoad = true;
+        final String userId = ParseUser.getCurrentUser().getObjectId();
+        mAdapter = new ChatAdapter(ChatActivity.this, userId, mMessages);
+        rvChat.setAdapter(mAdapter);
+
+        // associate the LayoutManager with the RecylcerView
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatActivity.this);
+        rvChat.setLayoutManager(linearLayoutManager);
+
         // When send button is clicked, create message object on Parse
         btSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,20 +95,21 @@ public class ChatActivity extends AppCompatActivity {
                 Message message = new Message();
                 message.setBody(data);
                 message.setUserId(ParseUser.getCurrentUser().getObjectId());
-
                 message.saveInBackground(new SaveCallback() {
                     @Override
                     public void done(ParseException e) {
-                        if(e == null) {
-                            Toast.makeText(ChatActivity.this, "Successfully created message on Parse",
-                                    Toast.LENGTH_SHORT).show();
-                        } else {
-                            Log.e(TAG, "Failed to save message", e);
-                        }
+                        Toast.makeText(ChatActivity.this, "Successfully created message on Parse",
+                                Toast.LENGTH_SHORT).show();
+                        refreshMessages();
                     }
                 });
                 etMessage.setText(null);
             }
         });
+    }
+
+    // Query messages from Parse so we can load them into the chat adapter
+    void refreshMessages() {
+        // TODO:
     }
 }
